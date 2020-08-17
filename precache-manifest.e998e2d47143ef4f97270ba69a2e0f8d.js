7b2e8b34b40affe45c6e462ef86a8878af54b9b4
deekshaaa <deekshagarg2000@gmail.com>
2020-08-17 15:02:25 +0530

    Updates

---
+++ precache-manifest.e998e2d47143ef4f97270ba69a2e0f8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3721b362ff4432425ab76b68daeacefe",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0cf1d19e9f4b75d9c17b",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "a2172d6ae0e3a5913b54",
+    "url": "/portfolio/static/css/main.4690c6bf.chunk.css"
+  },
+  {
+    "revision": "0cf1d19e9f4b75d9c17b",
+    "url": "/portfolio/static/js/2.f7c69d95.chunk.js"
+  },
+  {
+    "revision": "b4be63e519f388173e2f4209d9f0a6dd",
+    "url": "/portfolio/static/js/2.f7c69d95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2172d6ae0e3a5913b54",
+    "url": "/portfolio/static/js/main.e5c6f126.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
